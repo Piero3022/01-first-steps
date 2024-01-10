ef6a83008d0196955fa8c2e22fa6bf2446af5af6
PIERO FREUNDT <freundtpiero@gmail.com>
2024-01-10 15:23:14 -0500

    primer commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+    title: 'SEO Contact',
+    description: 'SEO Title',
+};
+
+export default function ContactPage() {
+
+    return (
+        <>
+            <span className="text-7xl">Contact Page</span>
+        </>
+    )
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+    title: 'SEO Pricing',
+    description: 'SEO Title',
+};
+
+export default function PricingPage() {
+
+    return (
+        <>
+            <span className="text-7xl">Pricing</span>
+        </>
+    )
+}
